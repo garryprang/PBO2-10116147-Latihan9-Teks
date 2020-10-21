4874c7d665a5928ed1304883a68294f42ecfcca3
grryv <grryv@grry>
2020-10-21 19:42:24 +0700

    Tugas - Latihan Pertemuan 1 Garry Prang 10116147 PBO-2

---
+++ src/pbo2/pkg10116147/latihan9/teks/PBO210116147Latihan9Teks.java
@@ -0,0 +1,21 @@
+package pbo2.pkg10116147.latihan9.teks;
+
+public class PBO210116147Latihan9Teks {
+
+/**
+ * @author
+ * Nama                 : Garry Prang
+ * NIM                  : 10116147
+ * Kelas                : PBO-2
+ * Deskripsi Program    : Program untuk menampilkan teks.
+ */
+    
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + c);
+    }
+    
+}
